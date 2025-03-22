0857658a10fea3e704a9fcbd52faaeccbcbee142
Andrey-Sid <infoxhitech@gmail.com>
2025-03-22 15:42:20 +0300

    Первая попытка

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardContainer = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const locationsList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(card, deleteItem) {
+  const cardItem = cardContainer.querySelector('.card').cloneNode(true);
+  const cardPicture = cardItem.querySelector('.card__image');
+
+  cardPicture.src = card.link;
+  cardPicture.alt = card.name;
+  cardItem.querySelector('.card__title').textContent = card.name;
+
+  cardItem.querySelector('.card__delete-button').addEventListener('click', deleteItem);
+
+  return cardItem;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+  event.target.closest('.card').remove(); 
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(card => {
+  locationsList.append(createCard(card, deleteCard));
+});
